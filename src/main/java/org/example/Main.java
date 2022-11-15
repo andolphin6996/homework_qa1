e421696259be52434c10d5db2f9882c7cc9af4d1
Andolphin6996 <pavlovna2002-2002@mail.ru>
2022-11-15 21:48:50 +0300

    /....

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("I am learning Git!");
+        System.out.println("12345!");
     }
 }
